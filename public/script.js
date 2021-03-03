a24686dc8d932dcf506f4260ceb7ef2c6b299044
Chan Tsz Him Oscar <Oscar@Chans-MacBook-Pro-9.local>
2021-03-03 18:30:30 +0800

    FINAL?

--- public/script.js
+++ public/script.js
@@ -1,60 +1,61 @@
-$(".register").click((e) => {
-    e.preventDefault();
-    let username = $('#username').val()
-    let password = $('#password').val()
-    console.log(username)
-    console.log(password)
+$(document).ready(() => {
+    $(".register").click((e) => {
+        e.preventDefault();
+        let username = $('#username').val()
+        let password = $('#password').val()
+        console.log(username)
+        console.log(password)
 
-    $.ajax({
-        type: "POST",
-        url: `http://localhost:3000/login`,
-        data: { username: username, password: password },
-        success: function () {
-            console.log("success");
-        },
-    }).done(function () {
-        window.location.reload();
-    }).fail(function () {
-        console.log('failed')
-    })
-});
-
-$('.trash').click((e) => {
-    e.preventDefault();
-    console.log(e.target.dataset.deletebutton)
-    $.ajax({
-        type: "DELETE",
-        url: `http://localhost:3000/users/${e.target.dataset.deletebutton}`,
-        success: function () {
-            console.log("success");
-        },
-    }).done(function () {
-        window.location.reload();
-        console.log('done')
-    }).fail(function () {
-        console.log('failed')
-    })
-})
-
-$(".textarea").focusout((e) => {
-    e.preventDefault();
-
-    console.log(e.target.id)
-    console.log(e.target.value)
-
-    $.ajax({
-        url: `http://localhost:3000/users/${e.target.id}`,
-        type: "PUT",
-        data: { newdata: e.target.value },
-        success: function () {
-            console.log("success");
-        },
-    })
-        .done(function () {
+        $.ajax({
+            type: "POST",
+            url: `http://localhost:3000/login`,
+            data: { username: username, password: password },
+            success: function () {
+                console.log("success");
+            },
+        }).done(function () {
             window.location.reload();
-        })
-        .fail(function () {
+        }).fail(function () {
             console.log('failed')
         })
-});
+    });
 
+    $('.trash').click((e) => {
+        e.preventDefault();
+        console.log(e.target.dataset.deletebutton)
+        $.ajax({
+            type: "DELETE",
+            url: `http://localhost:3000/users/${e.target.dataset.deletebutton}`,
+            success: function () {
+                console.log("success");
+            },
+        }).done(function () {
+            window.location.reload();
+            console.log('done')
+        }).fail(function () {
+            console.log('failed')
+        })
+    })
+
+    $(".textarea").focusout((e) => {
+        e.preventDefault();
+
+        console.log(e.target.id)
+        console.log(e.target.value)
+
+        $.ajax({
+            url: `http://localhost:3000/users/${e.target.id}`,
+            type: "PUT",
+            data: { newdata: e.target.value },
+            success: function () {
+                console.log("success");
+            },
+        })
+            .done(function () {
+                window.location.reload();
+            })
+            .fail(function () {
+                console.log('failed')
+            })
+    });
+});
